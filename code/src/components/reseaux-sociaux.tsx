e710f1caf8424119cbdfca074d715d3da2c17abc
almhannalaith-a11y.email <almhannalaith@gmail.com>
2026-01-06 16:08:53 +0100

    modifie-icon

--- code/src/components/reseaux-sociaux.tsx
+++ code/src/components/reseaux-sociaux.tsx
@@ -1,13 +1,26 @@
 
-import { FaInstagram, FaTiktok, FaFacebook } from 'react-icons/fa';
+import { FaInstagram, } from 'react-icons/fa';
+import { TiSocialFacebookCircular } from "react-icons/ti";
+import { RiTiktokLine } from "react-icons/ri";
+import { FaXTwitter } from "react-icons/fa6";
+import style from "../assets/css/reseaux.module.css"
 
 const Reseaux = () => {
     return (
 
-   <div className="social-icons">
-          <FaInstagram className="icon" />
-          <FaTiktok className="icon" />
-          <FaFacebook className="icon" />
+        <div className={style["social-icons"]}>
+           <a href="http://https://www.instagram.com/">
+                <FaInstagram />
+            </a>
+          <a href="http://https://www.tiktok.com/">
+                <RiTiktokLine />
+               </a>
+          <a href="http://www.facebook.com/">
+                <TiSocialFacebookCircular className={style["icon-facebook"]} />
+            </a>
+            <a href="https://x.com/">
+                <FaXTwitter />
+                </a>
         </div>
         
     );
